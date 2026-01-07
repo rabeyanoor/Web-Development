c28e3f83d979dc13270cd3b030ca80c36c51f088
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-01-07 20:11:06 +0600

    explore for loop

--- JS-conditionals-tasks/free_Drinks.js
+++
@@ -1,8 +0,0 @@
-let burgerPrice = 60;
-let cokePrice;
-if (burgerPrice > 500) {
-  cokePrice = "free coke";
-} else {
-  cokePrice = 30;
-}
-console.log("Coke Price:", cokePrice);
--- js-array-tasks/Declare_an_array.js
+++
@@ -1,8 +0,0 @@
-let fruits =["mango","banana","orange","apple","grape"];
-console.log("3rd index fruit is: ",fruits[3]);
-let lengthOfFruits=fruits.length;
-console.log("Total number of fruits: ",lengthOfFruits);
-fruits.push("jackfruit");
-console.log("After adding jackfruit: ",fruits);
- fruits[2]="kiwi";
-console.log("After replacing orange with kiwi: ",fruits);
