d13867fd22dc35368b866b56d8cc40aa410f96cf
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-01-30 16:06:06 +0600

    added dom, Connect your JS file with html file

---
+++ Milestone-5:Integrate JavaScript/Module-25:tour-of-dom/.history/js/dom_20260130155618.js
@@ -0,0 +1,4 @@
+console.log('hello from JS');
+console.log(document.body);
+
+console.log(document.body);
--- Milestone-5:Integrate JavaScript/Module-25:tour-of-dom/js/second.js
+++
@@ -1,5 +0,0 @@
-console.log('second file')
-
-const sections = document.getElementsByClassName('section-item');
-console.log(sections)
-
