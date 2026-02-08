2cd66f6374c485df97ca601d6fecf48af2525242
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-08 13:00:19 +0600

    Arrow function, multiple parameters, function body

---
+++ Module-30: JS Recap and Basic ES6/arrowFunction.js
@@ -0,0 +1,69 @@
+// function sum(num1, num2) {
+//     return num1 + num2;
+
+// }
+// const result = sum(10, 20);
+// console.log(result)
+
+
+
+// const sum2 = (num1, num2) => {
+//     return num1 + num2;
+// };
+// console.log(sum2(10,20))
+
+
+
+// const sum = (num1) => {
+//     console.log("hello",num1)
+// }
+// sum(10)
+
+
+
+// const sum2 = function (num1,num2) {
+//     return num1 + num2;
+
+// }
+// console.log(sum2(10,20))
+
+
+
+
+// function sum(num1, num2) {
+    
+//     return num1 + num2;
+// }
+// // sum(10,30)
+// const sum2 = (num1, num2) => {
+//     return num1+num2
+// }
+
+
+const test = () => console.log("hello empty");
+
+const square = x => x * x;
+
+const sum = (num1, num2) => num1 + num2;
+
+
+
+// console.log(square(10))
+
+document.getElementById("title").addEventListener("click",
+    
+    function () {
+        console.log("hello")
+    }
+
+)
+
+document.getElementById("title").addEventListener("click",
+
+
+
+    () => {
+    console.log("hello")
+    }
+
+)
