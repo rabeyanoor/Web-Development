58aa20c98cc5a66975951b78af7771d3d6790285
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-08 09:15:09 +0600

    ES6 Intro difference between var, Let, and const

---
+++ Module-30: JS Recap and Basic ES6/index.js
@@ -0,0 +1,23 @@
+
+
+
+// const or let 
+
+let age = 10;
+age = 11;
+console.log(age)
+
+const countryName = "Bangladesh";
+
+// countryName = "BD";
+
+// function sum() {
+//     if (true) {
+//         const fname="ggg"
+//     }
+//     console.log(fname)
+// }
+
+
+
+
