1cc962eadef25fde3e8035f42cf881cf9f509e1a
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-09 22:12:18 +0600

    Keys, values, entries, delete, seal, freeze

---
+++ .history/Module-30: JS Recap and Basic ES6/object_20260209220319.js
@@ -0,0 +1,40 @@
+ 
+
+// Object.seal(person)
+// Object.freeze(person)
+
+// person.age = 20;
+// const person = {
+//     name: "hena",
+//     age: 10,
+//     country:"BD"
+// }
+
+
+
+
+// console.log(Object.keys(person))
+// console.log(Object.values(person))
+// console.log(Object.entries(person))
+
+
+
+
+
+const person = {
+    name: "hena",
+    age: 10,
+    country:"BD"
+}
+
+// console.log(person["name"])
+
+// for (let key in person) {
+//    console.log(` key : ${key}  value: ${person[key]}`)
+// }
+// console.log(Object.entries(person))
+
+for (let [key,value] of Object.entries(person)) {
+    console.log(` key : ${key} value : ${value}`)
+}
+
---
+++ .history/Module-30: JS Recap and Basic ES6/object_20260209221033.js
@@ -0,0 +1,36 @@
+ // person.age = 20;
+// const person = {
+//     name: "hena",
+//     age: 10,
+//     country:"BD"
+// }
+
+// console.log(Object.keys(person))
+// console.log(Object.values(person))
+// console.log(Object.entries(person))
+
+
+// Object.seal(person)
+// Object.freeze(person) 
+// person.status = "not found";
+// console.log(person)
+
+
+
+const person = {
+    name: "hena",
+    age: 10,
+    country:"BD"
+}
+
+// console.log(person["name"])
+
+// for (let key in person) {
+//    console.log(` key : ${key}  value: ${person[key]}`)
+// }
+// console.log(Object.entries(person))
+
+for (let [key,value] of Object.entries(person)) {
+    console.log(` key : ${key} value : ${value}`)
+}
+
