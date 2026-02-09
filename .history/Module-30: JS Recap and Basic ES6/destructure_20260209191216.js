e446e8f24312d6b352a601e6236a4a4b78231752
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-09 19:29:02 +0600

    Object and Array Destructuring

---
+++ .history/Module-30: JS Recap and Basic ES6/destructure_20260209191216.js
@@ -0,0 +1,32 @@
+const hena = {
+    name: 'hena',
+    age: 10,
+    friends: ["hero alom", "bappa"],
+    status:"not found"
+}
+// const henStatus = hena.status;
+// const henName = hena.name;
+// const henAge = hena.age;
+// const henaFriends = hena.friends;
+
+// const { name, age } = hena;
+
+// console.log(age)
+
+
+
+const person = ["hero alom",{name:"gias"}, "hena", "bapparaj"];
+
+
+
+// const [nayok,obj] = person;
+// console.log(obj)
+
+
+const [test,obj,name,sadNayok] = person;
+console.log(sadNayok)
+
+
+
+
+
---
+++ .history/Module-30: JS Recap and Basic ES6/destructure_20260209192814.js
@@ -0,0 +1,31 @@
+const hena = {
+    name: 'hena',
+    age: 10,
+    friends: ["hero alom", "bappa"],
+    status:"not found"
+}
+// const henStatus = hena.status;
+// console.log(henStatus)
+// const henName = hena.name;
+// const henAge = hena.age;
+// const henaFriends = hena.friends;
+
+// const { name, age,friends,status } = hena;
+
+// console.log(age)
+
+
+
+const person = ["hero alom",{name:"gias"}, "hena", "bapparaj"];
+// const [nayok,obj] = person;// 
+
+// console.log(obj)
+
+
+const [test,obj,name,sadNayok] = person;
+console.log(sadNayok)
+
+
+
+
+
