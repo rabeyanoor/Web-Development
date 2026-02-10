b0de5c71edf7714da10f8ba625d674c89dbca74d
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-10 14:13:25 +0600

    added Basic ES6 Recap

---
+++ Module-31: (advanced) More ES6/.history/es6_20260210140223.js
@@ -0,0 +1,24 @@
+// const and let
+
+const person = {
+  name: "gias",
+  age: 12
+};
+
+const { age, name } = person;
+cons
+
+// array
+const friends = ["rahim", "Bangladesh", "krim"];
+
+// array destructuring
+const [frist, second] = friends;
+
+// spread operator
+const newFriends = ["hero alom", ...friends];
+// console.log(newFriends);
+
+// template string
+console.log(`
+amar shonar ${friends[1]} ami tomai valo bashi
+`);
---
+++ Module-31: (advanced) More ES6/.history/es6_20260210141203.js
@@ -0,0 +1,28 @@
+// const and let
+
+const person = {
+  name: "gias",
+  age: 12
+};
+
+delete.person
+console.log(Object.keys(person))
+
+const { age, name } = person;
+console .log(age,name)
+
+// array
+const friends = ["rahim", "Bangladesh", "krim"];
+
+// array destructuring
+const [frist, second] = friends;
+console.log(frist)
+
+// spread operator
+const newFriends = ["hero alom", ...friends];
+// console.log(newFriends);
+
+// template string
+console.log(`
+amar shonar ${friends[1]} ami tomai valo bashi
+`);
