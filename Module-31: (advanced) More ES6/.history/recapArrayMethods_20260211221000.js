af5c0344032ad3188906c180e4c421d789eb30e5
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-11 23:13:32 +0600

    Map, forEach, filter, find, reduce

---
+++ Module-31: (advanced) More ES6/.history/recapArrayMethods_20260211221000.js
@@ -0,0 +1,9 @@
+const products=[
+    {id:1,name:"apple",color:"black",price:1200,brand:"apple"},
+    {id:2,name:"samsung",color:"gold",price:9000,brand:"apple"},
+    {id:3,name:"xiaomi",color:"gold",price:100,brand:"xiaomi"}
+];
+
+const newProducts=products.map(p=>{
+    if()
+})
---
+++ Module-31: (advanced) More ES6/.history/recapArrayMethods_20260211230959.js
@@ -0,0 +1,20 @@
+const products=[
+    {id:1,name:"apple",color:"black",price:1200,brand:"apple"},
+    {id:2,name:"samsung",color:"gold",price:9000,brand:"apple"},
+    {id:3,name:"xiaomi",color:"gold",price:100,brand:"xiaomi"}
+];
+
+const newProducts=products.map(p=>{
+    if(p.band==="apple"){
+        p.price=p.price+100;
+    }
+    return p;
+})
+console.log(newProducts)
+
+
+products.forEach(product=>console.log(product))
+
+
+
+
