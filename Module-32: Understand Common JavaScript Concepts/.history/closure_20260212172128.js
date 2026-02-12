3a9e110c98dbe15096854854a3add98575644982
rabeyanoor <rabeyanoor.dev@gmail.com>
2026-02-12 17:22:20 +0600

    Explore about Closure

---
+++ Module-32: Understand Common JavaScript Concepts/.history/closure_20260212172128.js
@@ -0,0 +1,17 @@
+function sum() {
+  let counter = 0;
+  return function () {
+    counter++;
+    console.log(counter)
+  }
+}
+
+const result = sum()
+result()
+result()
+result()
+result()
+
+const result2 = sum()
+result2()
+result2()
